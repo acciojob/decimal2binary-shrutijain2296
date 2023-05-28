3e30e8d1c839857aa21bba6e19612af8de3c7d9a
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-28 23:31:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function threeSum(arr, target) {
+function decimalToBinary(n) {
 // write your code here
-  
+	let ans = 0;
+	while(n > 0){
+		const rem = n%2;
+		ans = rem + ans;
+		n = Math.floor(n/2);
+	}
+	return ans;
 }
 
 module.exports = threeSum;
