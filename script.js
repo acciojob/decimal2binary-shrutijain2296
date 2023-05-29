bda06239d7706a33fabb00f82afbf03f2a02c9b1
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-29 12:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function decimalToBinary(decimalNum) {
 // write your code here
 	let binaryStr = '';
-	while(n > 0){
+	while(decimalNum > 0){
 		const rem = decimalNum % 2;
 		binaryStr = rem.toString() + binaryStr; 
 		decimalNum = Math.floor(decimalNum/2);
 	}
 	return binaryStr;
 }
-
-module.exports = threeSum;
+module.exports = decimalToBinary;
+// module.exports = threeSum;
